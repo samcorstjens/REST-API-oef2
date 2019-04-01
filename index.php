c8afc020aebcaed2911a47a4202c44a7f9c070dc
samcorstjens <corstsam@gmail.com>
2019-04-01 12:03:53 +0200

    Oefening2

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>PHP DATABASE GET oefening 2</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <div class="container">
+   <div align="right" style="margin-bottom:5px;">
+   </div>
+
+   <div class="table-responsive">
+    <table class="table table-bordered table-striped">
+     <thead>
+      <tr>
+       <th>Restaurantnaam</th>
+       <th>Sterren</th>
+      </tr>
+     </thead>
+     <tbody></tbody>
+    </table>
+   </div>
+  </div>
+ </body>
+</html>
+<script type="text/javascript">
+
+</script>
